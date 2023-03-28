360cc5a2a7a2d67f3527dd69b3ba66ac68bd3b43
colspan <collin@collindigm.com>
2023-03-28 22:07:46 +0300

    Content Creator: Project Initialization

---
+++ src/test/java/com/addaboy/content/calendar/ApplicationTest.java
@@ -0,0 +1,15 @@
+package com.addaboy.content.calendar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApplicationTest {
+
+	/*
+	@Test
+	void contextLoads() {
+	}
+	*/
+
+}
