9cb12b83e05402031b188eee341b8d5f0849738e
colspan <collin@collindigm.com>
2023-03-29 08:18:46 +0300

    Controller: Accept Request and Response Class

---
+++ src/main/java/com/addaboy/content/calendar/controller/ContentController.java
@@ -0,0 +1,26 @@
+package com.addaboy.content.calendar.controller;
+
+import com.addaboy.content.calendar.model.Content;
+import com.addaboy.content.calendar.repository.ContentCollectionRepository;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping(name = "/api/content")
+public class ContentController {
+
+    public final ContentCollectionRepository repository;
+
+    public ContentController(ContentCollectionRepository repository) {
+        this.repository = repository;
+    }
+
+    // Make a request and find all the pieces of content in the system
+    @GetMapping(name = "")
+    public List<Content> getAll() {
+        return repository.findAll();
+    }
+}
