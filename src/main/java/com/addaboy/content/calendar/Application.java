389a282b40bb7edb00d58cfb121d2a05b5af3249
colspan <collin@collindigm.com>
2023-04-09 18:32:48 +0300

    Main class clean up

--- src/main/java/com/addaboy/content/calendar/Application.java
+++ src/main/java/com/addaboy/content/calendar/Application.java
@@ -1,27 +1,10 @@
 package com.addaboy.content.calendar;
 
-import org.springframework.boot.ConfigurableBootstrapContext;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.ConfigurableApplicationContext;
-import org.springframework.web.client.RestTemplate;
 
-import java.util.Arrays;
-
-@SpringBootApplication
 public class Application {
 
 	public static void main(String[] args) {
-		SpringApplication.run(Application.class, args);
 
-		/*
-		ConfigurableApplicationContext context = SpringApplication.run(Application.class, args);
-		Arrays.stream(context.getBeanDefinitionNames()).forEach(System.out::println);
-		RestTemplate restTemplate = (RestTemplate) context.getBean("restTemplate");
-
-		System.out.println(context.getBeanDefinitionCount());
-		System.out.println(restTemplate);
-		 */
 	}
 
 }
