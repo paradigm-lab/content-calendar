570f561869bc08930a9940addbee268fc0d2bdb1
colspan <collin@collindigm.com>
2023-04-13 13:14:45 +0300

    Repository: Defined our own query

--- src/main/java/com/addaboy/content/calendar/repository/ContentRepository.java
+++ src/main/java/com/addaboy/content/calendar/repository/ContentRepository.java
@@ -1,11 +1,20 @@
 package com.addaboy.content.calendar.repository;
 
 import com.addaboy.content.calendar.model.Content;
+import com.addaboy.content.calendar.model.Status;
+import org.springframework.data.jdbc.repository.query.Query;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.Repository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
     List<Content> findAllByTitleContains(String keyword);
+
+    @Query("""
+            SELECT * FROM Content
+            WHERE status = :status
+            """)
+    List<Content> listByStatus(@Param("status") Status status);
 }
