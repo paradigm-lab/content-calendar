579494be77c69416e0cd356a44972242a591f1d0
colspan <collin@collindigm.com>
2023-04-13 13:12:25 +0300

    Defined a derived query method to filter by title

--- src/main/java/com/addaboy/content/calendar/repository/ContentRepository.java
+++ src/main/java/com/addaboy/content/calendar/repository/ContentRepository.java
@@ -4,5 +4,8 @@ import com.addaboy.content.calendar.model.Content;
 import org.springframework.data.repository.ListCrudRepository;
 import org.springframework.data.repository.Repository;
 
+import java.util.List;
+
 public interface ContentRepository extends ListCrudRepository<Content, Integer> {
+    List<Content> findAllByTitleContains(String keyword);
 }
